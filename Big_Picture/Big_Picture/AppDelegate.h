e409f08c5484f0c954d58410dd5b2940b6de1a71
dhanesh939 <dhaneshpatel1234@gmail.com>
2014-07-25 23:26:48 -0400

    Enlarge  image
    Dhanesh Patel (C0634862)

---
+++ Big_Picture/Big_Picture/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Big_Picture
+//
+//  Created by DHANESH PATEL on 2014-07-25.
+//  Copyright (c) 2014 Group A of IPCT, Lambton College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Big_Picture/Big_Picture/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Big_Picture
+//
+//  Created by DHANESH PATEL on 2014-07-25.
+//  Copyright (c) 2014 Group A of IPCT, Lambton College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UIImageView *Picture;
+
+@end
